97cfe893c0761b5b29efee3999e82243bcc668fe
aidandearing <aidan.dearing@live.ca>
2015-03-30 09:22:23 -0400

    StaticObject.cs V1.1
    Fixed your horrendous { get; set; } semantics
    Adding blank spaces between methods
    Updated your libraries to the new Monogame 3.3 ones

--- Object.cs
+++ Object.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -13,55 +10,15 @@ namespace Object
 {
     public abstract class StaticObject
     {
-        protected Texture2D texture;
-        public Texture2D Texture
-        {
-            get { return texture; }
-            set { texture = value; }
-        }
-        protected Vector2 position;
-        public Vector2 Position
-        {
-            get { return position; }
-            set { position = value; }
-        }
-        protected Vector2 origin;
-        public Vector2 Origin
-        {
-            get { return origin; }
-            set { origin = value; }
-        }
-        
-        protected float scale;
-        public float Scale
-        {
-            get { return scale; }
-            set { scale = value; }
-        }
-        protected float rotation;
-        public float Rotation
-        {
-            get { return rotation; }
-            set { rotation = value; }
-        }
-        protected Color colour;
-        public Color Colour
-        {
-            get { return colour; }
-            set { colour = value; }
-        }
-        protected float depth;
-        public float Depth
-        {
-            get { return depth; }
-            set { depth = value; }
-        }
-        protected SpriteEffects spriteEffect;
-        public SpriteEffects SpriteEffect
-        {
-            get { return spriteEffect;}
-            set { spriteEffect = value; }
-        }
+        public Texture2D Texture { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Origin { get; set; }
+        public float Scale { get; set; }
+        public float Rotation { get; set; }
+        public Color Colour { get; set; }
+        public float Depth { get; set; }
+        public SpriteEffects SpriteEffect { get; set; }
+
         public StaticObject(Texture2D texture)
         {
             this.Texture = texture;
@@ -70,10 +27,12 @@ namespace Object
             this.Colour = Color.White;
             this.SpriteEffect = SpriteEffects.None;
         }
+        
         public void Update(GameTime gameTime, GraphicsDevice graphicsDevice)
         {
             Update(gameTime, graphicsDevice);
         }
+        
         public  void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             spriteBatch.Draw(Texture, Position, null, Colour, Rotation, Origin, Scale, SpriteEffect, Depth);
