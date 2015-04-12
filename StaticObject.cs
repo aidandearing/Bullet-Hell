2a56885bd614545766574817e0d66070b1915aa7
aidandearing <aidan.dearing@live.ca>
2015-04-11 23:12:58 -0400

    Update StaticObject.cs

--- StaticObject.cs
+++ StaticObject.cs
@@ -1,43 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 
-namespace Object
+namespace ActionGame
 {
     public abstract class StaticObject
     {
         public Texture2D Texture { get; set; }
         public Vector2 Position { get; set; }
         public Vector2 Origin { get; set; }
-        public float Scale { get; set; }
+        public Vector2 Scale { get; set; }
         public float Rotation { get; set; }
         public Color Colour { get; set; }
         public float Depth { get; set; }
         public SpriteEffects SpriteEffect { get; set; }
+        public string ID { get; set; }
+        public float Opacity { get; set; }
 
         public StaticObject(Texture2D texture)
         {
             this.Texture = texture;
-            this.Origin = new Vector2(texture.Width/2, texture.Height/2);
-            this.Scale = 1;
+            this.Origin = new Vector2(Texture.Width / 2, Texture.Height / 2);
+            this.Scale = new Vector2(1,1);
             this.Colour = Color.White;
+            this.Opacity = 1;
             this.SpriteEffect = SpriteEffects.None;
+            Guid id = Guid.NewGuid();
+            this.ID = id.ToString();
         }
-        
-        public void Update(GameTime gameTime, GraphicsDevice graphicsDevice)
-        {
-            Update(gameTime, graphicsDevice);
-        }
-        
-        public  void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+
+        public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Texture, Position, null, Colour, Rotation, Origin, Scale, SpriteEffect, Depth);
-            spriteBatch.End();
+        }
+
+        public static void LoadContent(ContentManager Content)
+        {
+            // NAMEOFCLASS.Textures.Add("name", Content.Load<Texture2D>("reference"));
         }
     }
 }
-
