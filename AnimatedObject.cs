d2bbce459c9c1d3aee371d55be9fbec54e15f4b5
Diadact <liamjohnston266@hotmail.ca>
2015-04-21 15:46:20 -0400

    Create AnimatedObject.cs

---
+++ AnimatedObject.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ActionGame
+{
+    abstract class AnimatedObject : DynamicObject
+    {
+        public Dictionary<string, Animation> Animations = new Dictionary<string, Animation>();
+
+        public string Animation { get; set; }
+
+        public AnimatedObject(Texture2D texture):base(texture)
+        {
+            this.Animation = "idle";
+        }        
+        new public void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            Animations[Animation].Update(gameTime);
+        }
+        new public void Draw(SpriteBatch spriteBatch)
+        {
+            Animations[Animation].Draw(spriteBatch, this);
+        }
+        public void AddAnimation(Animation animation)
+        {
+            Animations.Add(animation.Name, animation);
+        }
+    }
+}
