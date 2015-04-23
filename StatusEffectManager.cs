998628bac898301a52f1c6776140bad290fb3089
aidandearing <aidan.dearing@live.ca>
2015-04-23 01:38:59 -0400

    Update StatusEffectManager.cs
    Going along with other changes across the board to support the new abilities rolling out, this has been updated to function with more powerful statuseffects.

--- StatusEffectManager.cs
+++ StatusEffectManager.cs
@@ -1,7 +1,4 @@
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Xml;
 using System.Collections.Generic;
 
 namespace ActionGame
@@ -9,58 +6,55 @@ namespace ActionGame
     class StatusEffectManager
     {
         public List<StatusEffect> effects;
-        public object Parent { get; set; }
+        public DynamicObject Parent { get; set; }
+        public bool hasReflect { get; set; }
 
-        public StatusEffectManager(List<StatusEffect> effects, object Parent)
+        public StatusEffectManager(List<StatusEffect> effects, DynamicObject parent)
         {
             this.effects = effects;
+            this.Parent = parent;
         }
 
         public void AddEffect(StatusEffect effect)
         {
-            effect.Creator = this.Parent;
+            effect.Parent = (Entity)(this.Parent);
             this.effects.Add(effect);
         }
 
         public void AddEffect(List<StatusEffect> effects)
         {
             foreach (StatusEffect effect in effects)
+            {
                 this.effects.Add(effect);
+            }
         }
 
-        public void Update(GameTime gameTime, bool onUpdate, Entity parent, Projectile damageSource)
+        public void Update(GameTime gameTime, Entity parent, Projectile damageSource)
         {
             List<StatusEffect> deadEffects = new List<StatusEffect>();
+            hasReflect = false;
 
             foreach (StatusEffect effect in effects)
             {
                 // If the effect is onUpdate and the EffectManager.Update() are being called inside the update method, then the effect should update
-                if (effect.onUpdate && onUpdate)
-                {
-                    if (effect.onMove && parent.isMoving)
-                        effect.Update(gameTime, parent);
-                    else if (effect.onCast && parent.isCasting)
-                        effect.Update(gameTime, parent);
-                    else if (effect.onAttack && parent.isAttacking)
-                        effect.Update(gameTime, parent);
-                    else
-                        effect.Update(gameTime,parent);
-                }
-                else if (!effect.onUpdate && !onUpdate)
-                {
-                    // If the effect is reflect then it uses a special update
-                    if (effect.Name == "reflect" && damageSource != null)
-                        effect.Update(gameTime, parent, damageSource);
-                    else // If not it doesn't
-                        effect.Update(gameTime, parent);
-                }
+                if (effect.When == StatusEffect.StatusUpdate.update)
+                    effect.Update(gameTime, parent);
+                else if (effect.When == StatusEffect.StatusUpdate.move && parent.isMoving)
+                    effect.Update(gameTime, parent);
+                else if (effect.When == StatusEffect.StatusUpdate.cast && parent.isCasting)
+                    effect.Update(gameTime, parent);
+                else if (effect.When == StatusEffect.StatusUpdate.attack && parent.isAttacking)
+                    effect.Update(gameTime, parent);
+                else if (effect.When == StatusEffect.StatusUpdate.change && parent.isChangingAttr)
+                    effect.Update(gameTime,parent);
+
                 
                 if (effect.Over)
                     deadEffects.Add(effect);
             }
             foreach(StatusEffect effect in deadEffects)
             {
-                effects.RemoveAt(effects.IndexOf(effect));
+                effects.Remove(effect);
             }
         }
     }
