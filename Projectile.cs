bfa61889e31103e724a3837f5d4a8d831b4c672b
aidandearing <aidan.dearing@live.ca>
2015-04-27 18:28:41 -0400

    Update Projectile.cs
    Massively improved Projectile.cs constructor, begin, update, and collision logic. Now functions as intended, and works seemlessly with ability importing.

--- Projectile.cs
+++ Projectile.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 using System;
 using System.Collections.Generic;
 
@@ -11,12 +12,17 @@ namespace ActionGame
         public static Dictionary<string, Texture2D> Textures = new Dictionary<string, Texture2D>();
 
         private List<Entity> exclusionList;
-        public List<ParticleEmitter> emitters;
+        public List<ParticleEmitter> Emitters;
+        public List<StatusEffect> Effects;
+        public List<LightSource> Lights;
+        public List<SoundEffect> SoundEffects;
+        public List<Sound> Sounds;
 
         public Vector2 PositionOrigin { get; set; }
         public Entity Parent { get; set; }
         public object Target { get; set; }
         public float Range { get; set; }
+        public float RangeMax { get; set; }
         public float Lifetime { get; set; }
         private float LifetimeMax { get; set; }
         public Vector2 ScaleEnd { get; set; }
@@ -27,9 +33,8 @@ namespace ActionGame
         private bool isOriented { get; set; }
         private float Angle { get; set; }
         private float Speed { get; set; }
-        public bool isHostile { get; set; }
 
-        public StatusEffectManager effectManager;
+        private bool first = true;
 
         /// <summary>
         /// The Constructor for a projectile, this should never be used directly anywhere in code, save inside ProjectileManager
@@ -38,32 +43,36 @@ namespace ActionGame
         /// <param name="range">The range in pixels of the projectile</param>
         /// <param name="speed">The speed in pixels/second of the projectile</param>
         /// <param name="angle">The direction the projectile is launched</param>
-        /// <param name="isOriented">If the direction is oriented to the creator or not</param>
         /// <param name="lifetime">The lifetime in seconds the projectile stays alive for</param>
         /// <param name="parent">The creator of the projectile</param>
         /// <param name="scale">The scale of the projectile at start</param>
         /// <param name="scaleend">The scale of the projectile at end</param>
-        public Projectile(string key, bool ishostile, float range, float speed, float angle, bool isOriented, float lifetime, Vector2 scale, Vector2 scaleend, bool scalecurved, float opacity, float opacityend, bool opacitycurved, Entity parent) : base(Textures[key])
+        public Projectile(string key, float range, float speed, float angle, bool isOriented, float lifetime, Vector2 scale, Vector2 scaleend, bool scalecurved, float opacity, float opacityend, bool opacitycurved, Entity parent) : base(Textures[key])
         {
-            this.isHostile = ishostile;
-            this.Range = range;
+            this.Range = WorldSpace.MetresToPixels(range);
+            this.RangeMax = this.Range;
             this.Lifetime = lifetime;
             this.LifetimeMax = this.Lifetime;
             this.Parent = parent;
             this.isOriented = isOriented;
             this.Angle = angle;
-            this.Speed = speed;
-            this.Scale = scale;
-            this.ScaleEnd = scaleend;
+            this.Speed = WorldSpace.MetresToPixels(speed);
+            float scaleFloat = WorldSpace.TILESIZE / this.Texture.Width * scale.X;
+            this.Scale = Vector2.One * scaleFloat;
+            float scaleFloatEnd = WorldSpace.TILESIZE / this.Texture.Width * scaleend.X;
+            this.ScaleEnd = Vector2.One * scaleFloatEnd;
             this.ScaleCurved = scalecurved;
             this.Opacity = opacity;
             this.OpacityEnd = opacityend;
             this.OpacityCurved = opacitycurved;
-            this.effectManager = new StatusEffectManager(new List<StatusEffect>(),this);
             this.Depth = 0.5f;
             this.isCollidable = true;
             exclusionList = new List<Entity>();
-            emitters = new List<ParticleEmitter>();
+            Effects = new List<StatusEffect>();
+            Emitters = new List<ParticleEmitter>();
+            Lights = new List<LightSource>();
+            Sounds = new List<Sound>();
+            SoundEffects = new List<SoundEffect>();
             if (Parent != null)
                 Begin();
         }
@@ -75,8 +84,8 @@ namespace ActionGame
             this.diesOnImpact = projectile.diesOnImpact;
             this.ID = projectile.ID;
             this.isOriented = projectile.isOriented;
-            this.Lifetime = projectile.Lifetime;
-            this.LifetimeMax = this.Lifetime;
+            this.Lifetime = projectile.LifetimeMax;
+            this.LifetimeMax = projectile.LifetimeMax;
             this.Opacity = projectile.Opacity;
             this.OpacityEnd = projectile.OpacityEnd;
             this.OpacityCurved = projectile.OpacityCurved;
@@ -84,7 +93,8 @@ namespace ActionGame
             this.Parent = projectile.Parent;
             this.Position = projectile.Position;
             this.PositionOrigin = projectile.PositionOrigin;
-            this.Range = projectile.Range;
+            this.Range = projectile.RangeMax;
+            this.RangeMax = projectile.RangeMax;
             this.Rotation = projectile.Rotation;
             this.Scale = projectile.Scale;
             this.ScaleEnd = projectile.ScaleEnd;
@@ -97,53 +107,86 @@ namespace ActionGame
             this.Depth = 0.5f;
             this.isCollidable = true;
             exclusionList = new List<Entity>();
-            effectManager = new StatusEffectManager(new List<StatusEffect>(), projectile.Parent);
+            Effects = new List<StatusEffect>();
+            Lights = new List<LightSource>();
+            if (Parent != null)
+                Begin();
         }
 
         public void Begin()
         {
             float x, y;
+
             if (isOriented)
-            {
                 Angle += Parent.Rotation;
+
+            if (Speed > 0)
+            {
                 x = Speed * (float)Math.Cos(Angle);
                 y = Speed * (float)Math.Sin(Angle);
+                this.Velocity = new Vector2(x, y);
             }
             else
             {
-                x = Speed * (float)Math.Cos(Angle);
-                y = Speed * (float)Math.Sin(Angle);
+                x = Range * (float)Math.Cos(Angle);
+                y = Range * (float)Math.Sin(Angle);
+                this.Rotation = (float)Math.Atan2(Parent.Position.Y - y, Parent.Position.X - x);
+                this.Position = Parent.Position + new Vector2(x, y);
             }
-            this.Velocity = new Vector2(x, y);
+
+            foreach (ParticleEmitter emitter in Emitters)
+            {
+                ParticleManager.Instance().AddEmitter(emitter);
+            }
+
+            foreach (LightSource light in Lights)
+            {
+                LightManager.Instance().AddLight(light);
+            }
         }
 
         new public void Update(GameTime gameTime)
         {
+            if (first)
+            {
+                Begin();
+                first = false;
+            }
+
+            Velocity = new Vector2(Speed * (float)Math.Cos(Angle), Speed * (float)Math.Sin(Angle));
+            Rotation = (float)Math.Atan2(Velocity.Y, Velocity.X);
+
             base.Update(gameTime);
 
             // Update Logic
             float timelapse = gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+            float percentLife = 0;
 
-            Range -= Velocity.Length() * timelapse;
+            Range = Range - Velocity.Length() * timelapse;
             Lifetime -= timelapse;
 
-            if (ScaleCurved)
-            {
-                Scale = new Vector2(MathHelper.SmoothStep(ScaleEnd.X, Scale.X, Lifetime / LifetimeMax), MathHelper.SmoothStep(ScaleEnd.Y, Scale.Y, Lifetime / LifetimeMax));
-            }
+            if (LifetimeMax > 0)
+                percentLife = Lifetime / LifetimeMax;
             else
-                Scale += (ScaleEnd - Scale) / Lifetime * timelapse;
-            if (OpacityCurved)
-            {
-                Opacity = MathHelper.SmoothStep(OpacityEnd, Opacity, Lifetime / LifetimeMax);
-            }
-            else
-                Opacity += (OpacityEnd - Opacity) / Lifetime * timelapse;
+                percentLife = Range / RangeMax;
 
-            if (Range < 0 || Lifetime < 0)
-            {
+            if (ScaleCurved)
+                Scale = Vector2.SmoothStep(ScaleEnd, Scale, percentLife);
+            else
+                Scale = Vector2.Lerp(ScaleEnd, Scale, percentLife);
+
+            if (OpacityCurved)
+                Opacity = MathHelper.SmoothStep(OpacityEnd, Opacity, percentLife);
+            else
+                Opacity = MathHelper.Lerp(OpacityEnd, Opacity, percentLife);
+
+            if (Speed > 0)
+                diesOnImpact = true;
+            else
+                diesOnImpact = false;
+
+            if (Range < 0 || (Lifetime < 0 && LifetimeMax > 0))
                 End();
-            }
 
             if (Target != null)
             {
@@ -169,6 +212,8 @@ namespace ActionGame
                     return;
             }
 
+            this.CollisionRadius = (int)this.Scale.X * Texture.Width / 2;
+
             List<StaticObject> collisionList = ObjectManager.Instance().CheckCollisionReady(this);
             foreach (StaticObject obj in collisionList)
             {
@@ -181,49 +226,92 @@ namespace ActionGame
         {
             Vector2 delta = Position - target.Position;
 
-            if (delta.Length() <= CollisionRadius + target.CollisionRadius)
+            if (target.isCollideRectangular)
             {
-                if (target is Entity && !exclusionList.Contains((Entity)target))
+                CollisionRectangle = new Rectangle((int)Math.Floor((double)Position.X), (int)Math.Floor((double)Position.Y), CollisionRectangle.Width, CollisionRectangle.Height);
+                target.CollisionRectangle = new Rectangle((int)Math.Floor((double)target.Position.X), (int)Math.Floor((double)target.Position.Y), target.CollisionRectangle.Width, target.CollisionRectangle.Height);
+
+                if (CollisionRectangle.Intersects(target.CollisionRectangle))
                 {
-                    Entity entity = (Entity)target;
-
-                    // Get the hostility between the parent of this projectile and the colliding entity
-                    FactionManager.Hostility hostility = FactionManager.GetHostility(Parent.Faction, entity.Faction);
-
-                    // Check the hostility
-                    switch (hostility)
-                    {
-                        case FactionManager.Hostility.Friendly:
-                            if (!isHostile) // If friendly and this projectile is not hostile add the effects
-                            {
-                                entity.effectManager.AddEffect(this.effectManager.effects);
-                                exclusionList.Add(entity);
-                            }
-                            break;
-                        default:
-                            if (isHostile) // If hostile and this projectile is hostile add the effects
-                            {
-                                entity.effectManager.AddEffect(this.effectManager.effects);
-                                exclusionList.Add(entity);
-                            }
-                            break;
-                    }
+                    if (target is Entity)
+                    { }
+                    else if (target is StaticObject)
+                        End();
                 }
+            }
+            else
+            {
+                if (delta.Length() <= CollisionRadius + target.CollisionRadius)
+                {
+                    if (target is Entity && !exclusionList.Contains((Entity)target))
+                    {
+                        Entity entity = (Entity)target;
 
-                if (this.diesOnImpact)
-                    End();
+                        // Get the hostility between the parent of this projectile and the colliding entity
+                        FactionManager.Hostility hostility = FactionManager.GetHostility(Parent.Faction, entity.Faction);
+
+                        // Check the hostility
+                        switch (hostility)
+                        {
+                            case FactionManager.Hostility.Friendly:
+                                foreach (StatusEffect effect in Effects)
+                                {
+                                    if (!effect.Hostile)
+                                        entity.effectManager.AddEffect(effect);
+                                }
+                                exclusionList.Add(entity);
+                                break;
+                            default:
+                                foreach (StatusEffect effect in Effects)
+                                {
+                                    if (effect.Hostile)
+                                        entity.effectManager.AddEffect(effect);
+                                }
+                                exclusionList.Add(entity);
+                                break;
+                        }
+                    }
+                    else if (target is StaticObject && diesOnImpact && !(target is DynamicObject || target is Projectile))
+                        End();
+                }
             }
         }
 
         new public static void LoadContent(ContentManager Content)
         {
-            Projectile.Textures.Add("projrootaoe", Content.Load<Texture2D>("Textures/noisy1"));
+            Projectile.Textures.Add("circlesharp", Content.Load<Texture2D>("Textures/Lights/circlesharp"));
+
+            Projectile.Textures.Add("gravitybolt", Content.Load<Texture2D>("Projectiles/gravitybolt"));
+
+            Projectile.Textures.Add("consecratedGround", Content.Load<Texture2D>("Projectiles/consecratedGround"));
+            Projectile.Textures.Add("paladinHammer", Content.Load<Texture2D>("Projectiles/pallyHammer"));
+            Projectile.Textures.Add("lightning", Content.Load<Texture2D>("Projectiles/pallyLightning"));
+            Projectile.Textures.Add("smallLightning", Content.Load<Texture2D>("Projectiles/smallLightning"));
+            Projectile.Textures.Add("slash", Content.Load<Texture2D>("Projectiles/slash"));
+            Projectile.Textures.Add("stronghold", Content.Load<Texture2D>("Projectiles/stronghold"));
+
+            Projectile.Textures.Add("arrow", Content.Load<Texture2D>("Projectiles/arrow_purple"));
+            Projectile.Textures.Add("xplarrow", Content.Load<Texture2D>("Projectiles/arrow_orange"));
+            Projectile.Textures.Add("iceLance", Content.Load<Texture2D>("Projectiles/iceLance"));
+            Projectile.Textures.Add("iceBlock", Content.Load<Texture2D>("Projectiles/iceBlock"));
+            Projectile.Textures.Add("fireBall", Content.Load<Texture2D>("Projectiles/fireBall"));
+            Projectile.Textures.Add("bola", Content.Load<Texture2D>("Projectiles/bola"));
+
         }
 
         public void End()
         {
             ProjectileManager projectileManager = ProjectileManager.Instance();
             // Dying logic goes here
+            foreach (LightSource light in Lights)
+            {
+                LightManager.Instance().RemoveLight(light);
+            }
+
+            foreach (ParticleEmitter emitter in Emitters)
+            {
+                ParticleManager.Instance().RemoveEmitter(emitter);
+            }
 
             // This goes at the end
             projectileManager.RemoveProj(this);
