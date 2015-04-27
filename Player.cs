7e44914cfdc1f87df6c550eb7c236614d734196e
aidandearing <aidan.dearing@live.ca>
2015-04-27 18:27:58 -0400

    Update Player.cs
    Updated Player.cs to ensure it functions as desired, massively overhauled input handling, and changed collision functionality. As well as managing health. Non-functioning death logic has been added as well.

--- Player.cs
+++ Player.cs
@@ -13,8 +13,11 @@ namespace ActionGame
         public float stat_distance_travelled { get; set; }
         public int stat_skills_used { get; set; }
         public int stat_enemies_killed { get; set; }
+
         float actionCooldown = 0;
         const float ACTIONCOOLDOWN = 0.2f;
+        float respawn = 0;
+        const float RESPAWN = 10;
 
         KeyListener input;
         PlayerIndex player;
@@ -24,6 +27,9 @@ namespace ActionGame
             input = new KeyListener(player);
             Depth = 0.5f;
             isCollidable = true;
+            Health = 500;
+            HealthMax = 500;
+            HealthRegen = 1;
         }
 
         new public void ChangeAttr(Entity.Attribute attr, float value)
@@ -31,12 +37,12 @@ namespace ActionGame
             switch (attr)
             {
                 case Entity.Attribute.health:
-                    if (canBeDamaged && value < 0)
+                    if (canBeDamaged && value < 0 && Health >= 0)
                     {
                         Health += value;
                         stat_damage_taken += value;
                     }
-                    else if (canBeHealed && value > 0)
+                    else if (canBeHealed && value > 0 && Health <= HealthMax)
                     {
                         Health += value;
                         stat_health_healed += value;
@@ -64,127 +70,94 @@ namespace ActionGame
 
             actionCooldown -= timelapse;
 
-            if (gamePad.IsConnected)
+            if (isAlive)
             {
-                if (actionCooldown <= 0)
+                if (gamePad.IsConnected)
                 {
-                    if (gamePad.Triggers.Right > 0.5f)
+                    if (actionCooldown <= 0)
                     {
-                        UseAbility(0);
-                        actionCooldown = ACTIONCOOLDOWN;
-                    }
-                    else if (gamePad.Triggers.Left > 0.5f)
-                    {
-                        if (UseAbility(1))
+                        if (gamePad.Triggers.Right > 0.5f)
                         {
-                            stat_skills_used++;
+                            UseAbility(0);
+                            actionCooldown = ACTIONCOOLDOWN;
+                        }
+                        else if (gamePad.Triggers.Left > 0.5f)
+                        {
+                            if (UseAbility(1))
+                                stat_skills_used++;
+                            actionCooldown = ACTIONCOOLDOWN;
+                        }
+                        else if (gamePad.Buttons.A == ButtonState.Pressed)
+                        {
+                            if (UseAbility(2))
+                                stat_skills_used++;
+                            actionCooldown = ACTIONCOOLDOWN;
+                        }
+                        else if (gamePad.Buttons.X == ButtonState.Pressed)
+                        {
+                            if (UseAbility(3))
+                                stat_skills_used++;
+                            actionCooldown = ACTIONCOOLDOWN;
+                        }
+                        else if (gamePad.Buttons.Y == ButtonState.Pressed)
+                        {
+                            if (UseAbility(4))
+                                stat_skills_used++;
+                            actionCooldown = ACTIONCOOLDOWN;
+                        }
+                        else if (gamePad.Buttons.B == ButtonState.Pressed)
+                        {
+                            if (UseAbility(5))
+                                stat_skills_used++;
                             actionCooldown = ACTIONCOOLDOWN;
                         }
                     }
-                    else if (gamePad.Buttons.A == ButtonState.Pressed)
-                    {
-                        if (UseAbility(2))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                    else if (gamePad.Buttons.X == ButtonState.Pressed)
-                    {
-                        if (UseAbility(3))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                    else if (gamePad.Buttons.Y == ButtonState.Pressed)
-                    {
-                        if (UseAbility(4))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                    else if (gamePad.Buttons.B == ButtonState.Pressed)
-                    {
-                        if (UseAbility(5))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
+
+                    float x = gamePad.ThumbSticks.Left.X;
+                    float y = -gamePad.ThumbSticks.Left.Y;
+
+                    Velocity = new Vector2(Speed * x, Speed * y);
+
+                    if (Velocity.Length() == 0 && gamePad.ThumbSticks.Right.Length() > 0)
+                        Rotation = (float)Math.Atan2((double)-gamePad.ThumbSticks.Right.Y, (double)gamePad.ThumbSticks.Right.X);
+                    else if (Velocity.Length() > 0)
+                        Rotation = (float)Math.Atan2((double)Velocity.Y, (double)Velocity.X);
                 }
+                else
+                {
+                    if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                        Velocity = new Vector2(Velocity.X, -Speed);
+                    if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                        Velocity = new Vector2(Speed, Velocity.Y);
+                    if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                        Velocity = new Vector2(Velocity.X, Speed);
+                    else if (!Keyboard.GetState().IsKeyDown(Keys.Up))
+                        Velocity = new Vector2(Velocity.X, 0);
+                    if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                        Velocity = new Vector2(-Speed, Velocity.Y);
+                    else if (!Keyboard.GetState().IsKeyDown(Keys.Right))
+                        Velocity = new Vector2(0, Velocity.Y);
 
-                float x = gamePad.ThumbSticks.Left.X;
-                float y = -gamePad.ThumbSticks.Left.Y;
+                    if (PreviousPosition != Position)
+                        Rotation = (float)Math.Atan2((double)Velocity.Y, (double)Velocity.X);
 
-                Velocity = new Vector2(Speed * x, Speed * y);
-
-                if (Velocity.Length() == 0 && gamePad.ThumbSticks.Right.Length() > 0)
-                    Rotation = (float)Math.Atan2((double)-gamePad.ThumbSticks.Right.Y, (double)gamePad.ThumbSticks.Right.X);
-                else if (Velocity.Length() > 0)
-                    Rotation = (float)Math.Atan2((double)Velocity.Y, (double)Velocity.X);
+                    if (Keyboard.GetState().IsKeyDown(Keys.Q))
+                        UseAbility(0);
+                }
             }
+
+            if (Health <= 0)
+            {
+                isAlive = false;
+            }
+
+            if (isAlive)
+                respawn = 0;
             else
             {
-                if (Keyboard.GetState().IsKeyDown(Keys.Up))
-                    Velocity = new Vector2(Velocity.X, -Speed);
-                if (Keyboard.GetState().IsKeyDown(Keys.Right))
-                    Velocity = new Vector2(Speed, Velocity.Y);
-                if (Keyboard.GetState().IsKeyDown(Keys.Down))
-                    Velocity = new Vector2(Velocity.X, Speed);
-                else if (!Keyboard.GetState().IsKeyDown(Keys.Up))
-                    Velocity = new Vector2(Velocity.X, 0);
-                if (Keyboard.GetState().IsKeyDown(Keys.Left))
-                    Velocity = new Vector2(-Speed, Velocity.Y);
-                else if (!Keyboard.GetState().IsKeyDown(Keys.Right))
-                    Velocity = new Vector2(0, Velocity.Y);
-
-                if (PreviousPosition != Position)
-                    Rotation = (float)Math.Atan2((double)Velocity.Y, (double)Velocity.X);
-                
-                if (actionCooldown <= 0)
-                {
-                    if (Keyboard.GetState().IsKeyDown(Keys.Space))
-                    {
-                        UseAbility(0);
-                        actionCooldown = ACTIONCOOLDOWN;
-                    }
-                    
-                    else if (Keyboard.GetState().IsKeyDown(Keys.Q))
-                    {
-                        if (UseAbility(1))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-
-                    else if (Keyboard.GetState().IsKeyDown(Keys.W))
-                    {
-                        if (UseAbility(2))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                    else if (Keyboard.GetState().IsKeyDown(Keys.E))
-                    {
-                        if (UseAbility(3))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                    else if (Keyboard.GetState().IsKeyDown(Keys.R))
-                    {
-                        if (UseAbility(4))
-                        {
-                            stat_skills_used++;
-                            actionCooldown = ACTIONCOOLDOWN;
-                        }
-                    }
-                }
+                respawn += timelapse;
+                if (respawn > RESPAWN)
+                    isAlive = true;
             }
 
             base.Update(gameTime);
